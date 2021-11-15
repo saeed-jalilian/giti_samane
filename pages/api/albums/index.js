4f81a5ae1e38043cc623394bfca6f136b174882d
saeed <jalilian.saeed75@gmail.com>
2021-11-15 16:21:54 +0330

    set redirects on api pages

--- pages/api/albums/index.js
+++ pages/api/albums/index.js
@@ -9,7 +9,10 @@ export default async (req, res) => {
       const resApi = await getWithHeaders(`${process.env.BackendApiUrl}/albums`, auth)
       return res.status(resApi.status).send(resApi.data)
     } catch (e) {
-      return res.status(e.response.status).send(e.response.data)
+      if (e.response.status === 403) {
+        res.redirect('308', '/403')
+      }
+      res.status(e.response.status).send(e.response.data)
     }
   } else if (req.method === 'POST') {
     try {
