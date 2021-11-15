95029a4c3c57dd4d98332199f7f2e023f90a6492
saeed <jalilian.saeed75@gmail.com>
2021-11-15 16:30:09 +0330

    edited fetcher

--- components/api/fetcher.js
+++ components/api/fetcher.js
@@ -1,5 +1,16 @@
 import http from "./axiosConfig";
+import Router from 'next/router'
 
-export const fetcher = (url, auth) => http.get(url, {headers: {'Authorization': `Basic ${auth}`}}).then(res => res.data)
+export const fetcher = (url, auth) => http.get(url, {headers: {'Authorization': `Basic ${auth}`}})
+    .then(res => res.data)
+    .catch(error => {
+      if (error.response.status === 403) {
+        Router.push('/403')
+      } else if (error.response.status === 404) {
+        Router.push('/404')
+      } else {
+        Router.push('/500')
+      }
+    })
 
 export const fetchWithToken = (url, auth) => http.get(url, {headers: {'Authorization': `Basic ${auth}`}}).then(res => res.data)
