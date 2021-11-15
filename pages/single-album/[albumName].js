71805ab5051f48f70bca8703ca461b4f34920fcb
saeed <jalilian.saeed75@gmail.com>
2021-11-15 15:12:08 +0330

    added visible to title modal

--- pages/single-album/[albumName].js
+++ pages/single-album/[albumName].js
@@ -213,6 +213,7 @@ const AlbumPage = ({initAlbum, initPictures, albumName}) => {
             onCancel={handleCancelTitleModal}
             onOk={() => titleForm.submit()}
             className='modal-container'
+            visible={isTitleModalVisible}
         >
           <Form
               className='modal-form'
